b50bfbbf00c86d0683afbbc20753e845ae28c60d
Jeff DeFond <jeff.defond@cleardata.com>
2016-12-02 16:38:47 -0800

    Update client to be callable, and portable

--- client.go
+++ client.go
@@ -6,7 +6,6 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
-	"time"
 
 	MQTT "github.com/eclipse/paho.mqtt.golang"
 )
@@ -16,7 +15,7 @@ var handler MQTT.MessageHandler = func(client MQTT.Client, msg MQTT.Message) {
 	fmt.Printf("MSG: %s\n", msg.Payload())
 }
 
-func main() {
+func Client() (c MQTT.Client) {
 	cert, err := ioutil.ReadFile("./6525356d25-certificate.pem.crt")
 	if err != nil {
 		panic(err)
@@ -29,59 +28,41 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-
 	ca, err := ioutil.ReadFile("./ca.pem")
 	if err != nil {
 		panic(err)
 	}
 	pool := x509.NewCertPool()
 	pool.AppendCertsFromPEM(ca)
-
 	conf := &tls.Config{
 		Certificates:       []tls.Certificate{pair},
 		RootCAs:            pool,
 		InsecureSkipVerify: true,
 	}
 	conf.BuildNameToCertificate()
-
 	//create a ClientOptions struct setting the broker address, clientid, turn
 	//off trace output and set the default message handler
 	opts := MQTT.NewClientOptions().AddBroker("***REMOVED***")
 	opts.SetTLSConfig(conf)
-	// opts.SetCleanSession(true)
-	opts.SetClientID("notmirach")
+	opts.SetClientID("jeffmirach")
 	opts.SetDefaultPublishHandler(handler)
-
 	//create and start a client using the above ClientOptions
-	c := MQTT.NewClient(opts)
+	c = MQTT.NewClient(opts)
 	token := c.Connect()
-
 	if token.Wait() && token.Error() != nil {
 		panic(token.Error())
 	}
-
-	//subscribe to the topic /go-mqtt/sample and request messages to be delivered
+	//subscribe to the topic mirach/data and request messages to be delivered
 	//at a maximum qos of zero, wait for the receipt to confirm the subscription
 	if token := c.Subscribe("mirach/data", 0, nil); token.Wait() && token.Error() != nil {
 		fmt.Println(token.Error())
 		os.Exit(1)
 	}
+	return c
+}
 
-	//Publish 5 messages to /go-mqtt/sample at qos 1 and wait for the receipt
-	//from the server after sending each message
-	for i := 0; i < 5; i++ {
-		text := fmt.Sprintf("this is msg #%d!", i)
-		token := c.Publish("mirach/data", 0, false, text)
-		token.Wait()
-	}
+func Publish(mes result, c MQTT.Client) {
 
-	time.Sleep(3 * time.Second)
-
-	//unsubscribe from /go-mqtt/sample
-	if token := c.Unsubscribe("mirach/data"); token.Wait() && token.Error() != nil {
-		fmt.Println(token.Error())
-		os.Exit(1)
-	}
-
-	c.Disconnect(250)
+	token := c.Publish("mirach/data", 0, false, mes)
+	token.Wait()
 }
