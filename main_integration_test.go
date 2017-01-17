683d7d5197ecc82f67b09cc4efbf12bf7775853e
Jeff DeFond <jeff.defond@cleardata.com>
2017-01-17 12:14:13 -0800

    Assert some meaningful things about asset registration TODO: time to be eve the evil listening customer

--- main_integration_test.go
+++ main_integration_test.go
@@ -5,7 +5,9 @@ package main
 import (
 	"flag"
 	"fmt"
-	// "github.com/stretchr/testify/assert"
+	"github.com/spf13/viper"
+	"github.com/stretchr/testify/assert"
+	"io/ioutil"
 	"os"
 	"os/exec"
 	"testing"
@@ -14,22 +16,34 @@ import (
 
 func TestMain(m *testing.M) {
 	flag.Parse()
-	shell("pwd")
+	os.Args = []string{"-v"}
+
 	//grab config from test resources
 	shell("cp", "test_resources/config.yaml", ".")
+
 	//grab test customer certs from test resources
 	shell("cp", "-r", "test_resources/customer", ".")
 	shell("cp", "test_resources/ca.pem", ".")
+
 	//run tests
 	run := m.Run()
+
 	//cleanup config
 	shell("rm", "config.yaml")
+
 	//cleanup customer-certs
 	shell("rm", "-r", "./customer")
 	shell("rm", "./ca.pem")
 	shell("rm", "-r", "/etc/mirach/asset")
+
 	os.Exit(run)
 }
+
+func load_test_config() {
+	shell("cp", "test_resources/config.yaml", ".")
+}
+
+// func clear_test_
 
 func shell(cmd string, args ...string) []byte {
 	out, err := exec.Command(cmd, args...).Output()
@@ -42,7 +56,8 @@ func shell(cmd string, args ...string) []byte {
 	return out
 }
 
-func TestIntegrationGetCustomerId(t *testing.T) {
+func TestIntegrationMainRegistration(t *testing.T) {
+	assert := assert.New(t)
 	timeChan := Timeout(15 * time.Second)
 	running := false
 	for {
@@ -53,9 +68,20 @@ func TestIntegrationGetCustomerId(t *testing.T) {
 		}
 		var timeout = <-timeChan
 		if timeout {
-			fmt.Println("reveived timeout")
-			return
+			fmt.Println("mirach has run for allotted time interupting and, checking assertions")
+			viper.SetConfigName("config")
+			viper.SetConfigType("yaml")
+			viper.AddConfigPath(".")
+			err := viper.ReadInConfig()
+			assert.Nil(err)
+			assert.Equal(viper.GetString("customer.id"), "00000666")
+			priv, err := ioutil.ReadFile("/etc/mirach/asset/keys/private.pem.key")
+			assert.Nil(err)
+			assert.NotEmpty(priv)
+			ca, err := ioutil.ReadFile("/etc/mirach/asset/keys/ca.pem.crt")
+			assert.Nil(err)
+			assert.NotEmpty(ca)
+			break
 		}
 	}
-
 }
