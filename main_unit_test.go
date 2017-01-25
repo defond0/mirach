ee6bfc713f8f2909586643e37f05c9325b28dd22
Jeff DeFond <jeff.defond@cleardata.com>
2017-01-24 17:41:57 -0800

    Update with a stub test for RunPlugin, still need to mock Token correctly

--- main_unit_test.go
+++ main_unit_test.go
@@ -6,54 +6,57 @@ import (
 	"testing"
 
 	jww "github.com/spf13/jwalterweatherman"
+	"github.com/spf13/viper"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestMainConfigureLogging(t *testing.T) {
+	Mirach = &mirachSession{}
 	assert := assert.New(t)
 	opts.Verbose = []bool{}
-	configureLogging()
-	assert.Equal(AppConfig.verbosity, 0, "Expected vebosity of 0")
+	Mirach.configureLogging()
+	assert.Equal(Mirach.getVerbosity(), 0, "Expected vebosity of 0")
 	assert.Equal(jww.StdoutThreshold(), jww.LevelError)
 	opts.Verbose = []bool{true}
-	configureLogging()
-	assert.Equal(AppConfig.verbosity, 1, "Expected vebosity of 1")
+	Mirach.configureLogging()
+	assert.Equal(Mirach.getVerbosity(), 1, "Expected vebosity of 1")
 	assert.Equal(jww.StdoutThreshold(), jww.LevelInfo)
 	opts.Verbose = []bool{true, true, true}
-	configureLogging()
-	assert.Equal(AppConfig.verbosity, 3, "Expected vebosity of 3")
+	Mirach.configureLogging()
+	assert.Equal(Mirach.getVerbosity(), 3, "Expected vebosity of 3")
 	assert.Equal(jww.StdoutThreshold(), jww.LevelTrace)
 }
 
 func TestSetConfigDirs(t *testing.T) {
 	// Just checking that configDirs is correct amalgam
+	Mirach = &mirachSession{}
 	assert := assert.New(t)
-	setConfigDirs()
-	assert.NotNil(AppConfig.sysConfDir)
-	assert.NotNil(AppConfig.userConfDir)
-	assert.Equal(AppConfig.configDirs, []string{".", AppConfig.userConfDir, AppConfig.sysConfDir})
+	Mirach.setConfigDirs()
+	assert.NotNil(Mirach.getSysConfDir())
+	assert.NotNil(Mirach.getUserConfDir())
+	assert.Equal(Mirach.getConfigDirs(), []string{".", Mirach.getUserConfDir(), Mirach.getSysConfDir()})
 }
 
-// func TestInitializeConfigAndLogging(t *testing.T) {
-// 	assert := assert.New(t)
-// 	configCalls := -1
-// 	getConfig := func() {
-// 		configCalls += 1
-// 	}
-// 	getConfig()
-// 	loggingCalls := -1
-// 	configureLogging := func() {
-// 		loggingCalls += 1
-// 	}
-// 	configureLogging()
-// 	confDirCalls := -1
-// 	setConfigDirs := func() {
-// 		confDirCalls += 1
-// 	}
-// 	setConfigDirs()
-
-// 	initializeConfigAndLogging()
-// 	assert.Equal(loggingCalls, 1)
-// 	assert.Equal(confDirCalls, 1)
-// 	assert.Equal(configCalls, 1)
-// }
+func TestRunPlugin(t *testing.T) {
+	viper.SetConfigName("config")
+	viper.Set("customer.id", "unit-test")
+	viper.Set("asset.id", "unit-test")
+	shell("cp", "./test_resources/unit-test.sh", ".")
+	defer shell("rm", "./unit-test.sh")
+	assert := assert.New(t)
+	test_plug := Plugin{}
+	test_plug.Label = "unit-test-plugin"
+	test_plug.Cmd = "./unit-test.sh"
+	test_plug.Schedule = "doesn't matter"
+	// c := new(mocks.Client)
+	// token := new(mocks.Token)
+	// token.On("Wait").Return()
+	// var qos uint8
+	// qos = 1
+	// c.On("Publish", "mirach/data/unit-test/unit-test", qos, false, "{\"type\":\"unit\",\"data\":\"test\"}").Return(token)
+	// out := RunPlugin(test_plug, c)
+	// assert.NotNil(out)
+	// out()
+	// token.AssertExpectations(t)
+	// c.AssertExpectations(t)
+}
