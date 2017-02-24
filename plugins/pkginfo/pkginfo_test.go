409a2fa7cc0e701b855ce9afafc00833c4bd31f6
Jeff DeFond <jeff.defond@cleardata.com>
2017-02-24 15:27:17 -0800

    Add tests for over all struct

---
+++ plugins/pkginfo/pkginfo_test.go
@@ -0,0 +1,118 @@
+// +build unit
+
+package pkginfo
+
+import (
+	"fmt"
+	"testing"
+
+	"encoding/json"
+
+	"cleardata.com/dash/mirach/plugins/pkginfo/parsers"
+)
+
+type MockInfoGroup struct {
+	mockGetInfo func()
+	mockString  func() string
+}
+
+func (m *MockInfoGroup) GetInfo() {
+	m.mockGetInfo()
+}
+
+func (m *MockInfoGroup) String() string {
+	return m.mockString()
+}
+
+func TestKbStatusGetString(t *testing.T) {
+	testInfo := make(map[string][]parsers.KBArticle)
+	testInfo["installed"] = []parsers.KBArticle{
+		{
+			Name:     "KB666",
+			Security: false,
+		},
+	}
+	testInfo["available"] = []parsers.KBArticle{
+		{
+			Name:     "KB667",
+			Security: true,
+		},
+	}
+	testInfo["available_security"] = []parsers.KBArticle{
+		{
+			Name:     "KB667",
+			Security: true,
+		},
+	}
+	ogKb := new(KBStatus)
+	mockKb := &MockInfoGroup{
+		mockGetInfo: func() {
+			fmt.Println("Get mock")
+			ogKb.Articles = testInfo
+		},
+		mockString: ogKb.String,
+	}
+	mockKb.GetInfo()
+	newKb := new(KBStatus)
+	if err := json.Unmarshal([]byte(mockKb.String()), &newKb); err != nil {
+		t.Error("can't unmarshall kbs")
+	}
+	if !(newKb.Articles["available"][0] == ogKb.Articles["available"][0]) {
+		t.Error("available kbs don't match")
+	}
+	if !(newKb.Articles["available_security"][0] == ogKb.Articles["available_security"][0]) {
+		t.Error("available_security kbs don't match")
+	}
+	if !(newKb.Articles["installed"][0] == ogKb.Articles["installed"][0]) {
+		t.Error("installed kbs don't match")
+	}
+
+}
+
+func TestPkgStatusGetString(t *testing.T) {
+	testInfo := make(map[string][]parsers.LinuxPackage)
+	testInfo["installed"] = []parsers.LinuxPackage{
+		{
+			Name:     "ssh",
+			Version:  "0.1.0",
+			Security: false,
+		},
+	}
+	testInfo["available"] = []parsers.LinuxPackage{
+		{
+			Name:     "ssh",
+			Version:  "0.1.2",
+			Security: true,
+		},
+	}
+	testInfo["available_security"] = []parsers.LinuxPackage{
+		{
+			Name:     "ssh",
+			Version:  "0.1.2",
+			Security: true,
+		},
+	}
+	ogKb := new(PkgStatus)
+	mockKb := &MockInfoGroup{
+		mockGetInfo: func() {
+			fmt.Println("Get mock")
+			ogKb.Packages = testInfo
+		},
+		mockString: ogKb.String,
+	}
+	mockKb.GetInfo()
+	newKb := new(PkgStatus)
+	if err := json.Unmarshal([]byte(mockKb.String()), &newKb); err != nil {
+		t.Error("can't unmarshall pkgs")
+	}
+	if !(newKb.Packages["available"][0] == ogKb.Packages["available"][0]) {
+		t.Error("available pkgs don't match")
+	}
+	if !(newKb.Packages["available_security"][0] == ogKb.Packages["available_security"][0]) {
+		t.Error("available_security pkgs don't match")
+	}
+	if !(newKb.Packages["installed"][0] == ogKb.Packages["installed"][0]) {
+		t.Error("installed pkgs don't match")
+	}
+
+}
