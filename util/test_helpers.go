d4a2f819c53cf0b035cc5bf61bdfb74c63c2c867
Herkermer Sherwood <herkermer.sherwood@cleardata.com>
2017-02-27 14:41:33 -0800

    Merge test_helpers from mirachlib into util

--- util/test_helpers.go
+++ util/test_helpers.go
@@ -2,46 +2,84 @@
 
 package util
 
-import (
-	"path/filepath"
+import "github.com/spf13/afero"
 
-	homedir "github.com/mitchellh/go-homedir"
-	"github.com/spf13/afero"
-)
+// OSFs is the operating system's filesystem.
+var OSFs = afero.NewOsFs()
 
-var osFs = afero.NewOsFs()
-var testFs = afero.NewMemMapFs()
+// TestFs is the in-memory filesystem used for testing.
+var TestFs = afero.NewMemMapFs()
 
-func writeTestCerts() {
-	ca, err := afero.ReadFile(osFs, "../test_resources/ca.pem")
+// ResetTestFs creates a new TestFs blank slate.
+func ResetTestFs() {
+	TestFs = afero.NewMemMapFs()
+}
+
+// WriteTestCerts writes certs to the test filesystem for testing.
+func WriteTestCerts() {
+	ca, err := afero.ReadFile(OSFs, "../test_resources/ca.pem")
 	if err != nil {
 		panic(err)
 	}
-	testCA, err := testFs.Create("/etc/mirach/ca.pem")
+	privKey, err := afero.ReadFile(OSFs, "../test_resources/customer/keys/private.pem.key")
+	if err != nil {
+		panic(err)
+	}
+	cert, err := afero.ReadFile(OSFs, "../test_resources/customer/keys/ca.pem.crt")
+	if err != nil {
+		panic(err)
+	}
+	testCA, err := TestFs.Create("/etc/mirach/ca.pem")
 	if err != nil {
 		panic(err)
 	}
 	defer testCA.Close()
-	_, err = testCA.WriteString(string(ca))
-	if err != nil {
-		panic(err)
-	}
-	// Write a keys to the $HOME directory to verify FindInDirs.
-	privKey, err := afero.ReadFile(osFs, "../test_resources/customer/keys/private.pem.key")
-	if err != nil {
-		panic(err)
-	}
-	home, err := homedir.Dir()
-	if err != nil {
-		panic(err)
-	}
-	testPrivKey, err := testFs.Create(filepath.Join(home, ".config/mirach/customer/keys/private.pem.key"))
+	testPrivKey, err := TestFs.Create("/etc/mirach/customer/keys/private.pem.key")
 	if err != nil {
 		panic(err)
 	}
 	defer testPrivKey.Close()
+	testCert, err := TestFs.Create("/etc/mirach/customer/keys/ca.pem.crt")
+	if err != nil {
+		panic(err)
+	}
+	defer testCert.Close()
+	_, err = testCA.WriteString(string(ca))
+	if err != nil {
+		panic(err)
+	}
 	_, err = testPrivKey.WriteString(string(privKey))
 	if err != nil {
 		panic(err)
 	}
+	_, err = testCert.WriteString(string(cert))
+	if err != nil {
+		panic(err)
+	}
+}
+
+func writeTestConfig(path string) {
+	config, err := afero.ReadFile(OSFs, path)
+	if err != nil {
+		panic(err)
+	}
+	testConfig, err := TestFs.Create("/etc/mirach/config.yaml")
+	if err != nil {
+		panic(err)
+	}
+	defer testConfig.Close()
+	_, err = testConfig.WriteString(string(config))
+	if err != nil {
+		panic(err)
+	}
+}
+
+// WriteTestEvilConfig writes a known bad configuration for testing.
+func WriteTestEvilConfig() {
+	writeTestConfig("../test_resources/eve_config.yaml")
+}
+
+// WriteTestGoodConfig writes a known good configuration for testing.
+func WriteTestGoodConfig() {
+	writeTestConfig("../test_resources/config.yaml")
 }
