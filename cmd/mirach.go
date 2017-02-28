c69ecf98ecaf5a5806f06dbf2982ab17519f411d
Herkermer Sherwood <herkermer.sherwood@cleardata.com>
2017-02-28 14:23:39 -0800

    Add version flag to display the version

--- cmd/mirach.go
+++ cmd/mirach.go
@@ -5,6 +5,7 @@ import (
 	"os"
 
 	"gitlab.eng.cleardata.com/dash/mirach/mirachlib"
+	"gitlab.eng.cleardata.com/dash/mirach/util"
 
 	"github.com/spf13/cobra"
 )
@@ -13,6 +14,7 @@ import (
 var (
 	infoGroup string
 	level     string
+	version   bool
 )
 
 // MirachCmd is the root mirach command.
@@ -21,8 +23,12 @@ var MirachCmd = &cobra.Command{
 	Short: "mirach collects data and send it over mqtt.",
 	Long: "mirach is a lightweight tool for collecting information about a " +
 		"computer then sending that information to an mqtt broker for consumption. " +
-		" The command alone runs the main mirach program.",
+		"The command alone runs the main mirach program.",
 	Run: func(cmd *cobra.Command, args []string) {
+		if version {
+			util.ShowVersion()
+			return
+		}
 		if err := mirachlib.SetLogLevel(level); err != nil {
 			fmt.Println(err)
 			os.Exit(1)
@@ -37,6 +43,8 @@ var MirachCmd = &cobra.Command{
 func init() {
 	MirachCmd.PersistentFlags().StringVarP(&level, "loglevel", "l", "error",
 		"log level: error, info, trace")
+	MirachCmd.Flags().BoolVar(&version, "version", false, "display current mirach version")
+
 	MirachCmd.AddCommand(compinfoCmd)
 	compinfoCmd.Flags().StringVarP(&infoGroup, "infogroup", "i", "system",
 		"compinfo group to check: docker, load, system")
