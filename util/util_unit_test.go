25cbc559e7418330f8891eb4ecd845c4abd3ebc7
Herkermer Sherwood <herkermer.sherwood@cleardata.com>
2017-03-08 11:31:12 -0800

    Add tests and examples for the new functions

--- util/util_unit_test.go
+++ util/util_unit_test.go
@@ -3,13 +3,14 @@
 package util
 
 import (
+	"fmt"
 	"os"
 	"path/filepath"
 	"testing"
 
 	"github.com/spf13/afero"
-	"github.com/theherk/viper"
 	"github.com/stretchr/testify/assert"
+	"github.com/theherk/viper"
 )
 
 func TestMain(m *testing.M) {
@@ -53,4 +54,51 @@ func TestGetConfig(t *testing.T) {
 	assert.Nil(err)
 	assert.NotNil(conf)
 	assert.Equal("00000666-mirach", viper.GetString("asset.id"), "value in read config")
+}
+
+func ExampleSplitAt() {
+	b, _ := SplitAt([]byte("abc ⌘ efg"), 5)
+	fmt.Println(b[1])
+	// Output: [140 152 32 101 102]
+}
+
+func TestSplitAt(t *testing.T) {
+	in := []byte("⌘⌘ test")
+	size := 5
+	b0 := []byte{226, 140, 152, 226, 140}
+	b1 := []byte{152, 32, 116, 101, 115}
+	b2 := []byte{116}
+	exp := [][]byte{b0, b1, b2}
+	out, err := SplitAt(in, size)
+	if err != nil {
+		t.Error("how even")
+	}
+	for x := range out {
+		for y := range out[x] {
+			if out[x][y] != exp[x][y] {
+				t.Error("slice split incorrectly: %v", out)
+			}
+		}
+	}
+}
+
+func ExampleSplitStringAt() {
+	s, _ := SplitStringAt("abc ⌘ efg", 5)
+	fmt.Println(s[1] + ", " + s[2])
+	// Output: ⌘ e, fg
+}
+
+func TestSplitStringAt(t *testing.T) {
+	in := "⌘⌘ test"
+	size := 5
+	exp := []string{"⌘", "⌘ t", "est"}
+	out, err := SplitStringAt(in, size)
+	if err != nil {
+		t.Error("how even")
+	}
+	for i := range out {
+		if out[i] != exp[i] {
+			t.Error("string split incorrectly: %v", out)
+		}
+	}
 }
