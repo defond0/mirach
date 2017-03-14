ab1a8b52b3ad45f8f145b3cea3fab0aad0ebedf4
Herkermer Sherwood <herkermer.sherwood@cleardata.com>
2017-03-14 13:22:03 -0700

    Add defer for plugin calls via cli / main

--- main.go
+++ main.go
@@ -3,11 +3,22 @@ package main
 import (
 	"fmt"
 	"os"
+	"reflect"
 
 	"gitlab.eng.cleardata.com/dash/mirach/cmd"
 )
 
 func main() {
+	defer func() {
+		if r := recover(); r != nil {
+			if reflect.TypeOf(r).String() == "plugin.Exception" {
+				fmt.Println(r)
+				return
+			}
+			fmt.Println(r)
+			os.Exit(1)
+		}
+	}()
 	if err := cmd.MirachCmd.Execute(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
