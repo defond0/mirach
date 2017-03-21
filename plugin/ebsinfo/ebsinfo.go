7406525bf4bf8773386dd3b09fcaa7f7a2dfec53
Jeff DeFond <jeff.defond@cleardata.com>
2017-03-21 16:51:25 -0700

    Begin stubbing ebs info plugin

---
+++ plugin/ebsinfo/ebsinfo.go
@@ -0,0 +1,20 @@
+package ebsinfo
+
+type EbsInfoGroup struct {
+}
+
+func (e *EbsInfoGroup) GetInfo() {
+
+}
+
+func (e *EbsInfoGroup) String() string {
+	return ""
+}
+
+func GetInfo() {
+
+}
+
+func String() string {
+	return ""
+}
