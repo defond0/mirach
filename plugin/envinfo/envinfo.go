b8370d266232d320fc5c1de419c48c6715248f61
Jeff DeFond <jeff.defond@cleardata.com>
2017-03-21 12:13:12 -0700

    Add envinfo tests and update shape so it fits plugin standards

--- plugin/envinfo/envinfo.go
+++ plugin/envinfo/envinfo.go
@@ -1,50 +1,99 @@
-package environment
+package envinfo
 
 import (
+	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"net/http"
 	"regexp"
+
+	"gitlab.eng.cleardata.com/dash/mirach/plugin"
+
+	jww "github.com/spf13/jwalterweatherman"
 )
 
-// InfoGroup is an interface for getting data and marshaling to json.
-type InfoGroup interface {
-	GetInfo()
-	String() string
+type EnvInfoGroup struct {
+	CloudProvider     string            `json:"provider"`
+	CloudProviderInfo map[string]string `json:"info"`
 }
 
-type EnvInfo struct {
-	CloudProvider     string
-	CloudProviderInfo map[string]string
-}
-
-//WhereAmI returns a string representing the name of the cloud provider, or err
-// if they do not use a cloud provider in the following list:
-//       aws
-func WhereAmI() (string, error) {
-	if ans, err := IAmInAws(); ans {
-		return "aws", err
-	}
-	return "none", nil
-}
-
-// IAmInAws returns a bool, is mirach in aws?
-func IAmInAws() (bool, error) {
-	res, err := http.Get("http://169.254.169.254/latest/meta-data/instance-id")
+func hitAwsMagicIp(path string) (string, error) {
+	res, err := http.Get(fmt.Sprintf("http://169.254.169.254/latest/meta-data/%s", path))
 	if err != nil {
-		return false, nil
+		return "", err
 	}
 	defer res.Body.Close()
 	if res.StatusCode == 200 { // OK
 		bodyBytes, err := ioutil.ReadAll(res.Body)
 		if err != nil {
-			return false, err
+			return "", err
 		}
-		// this will match instances ids we could force them to be i-(string of exeactly 17 characters)
-		// if they get longer like they have had a history of doing this will continue to work, essentially
-		// saying that they must be at least as long as they are currently.
-		matched, err := regexp.MatchString("^i-[[:alnum:]]{8,}$", string(bodyBytes))
-		return matched, err
+		return string(bodyBytes), err
 	}
-	return false, err
+	return "", err
+}
 
+// IAmInAws returns a bool, is mirach in aws?
+func IAmInAws() (bool, error) {
+	id, err := hitAwsMagicIp("instance-id")
+	if err != nil {
+		return false, err
+	}
+	// this will match instances ids we could force them to be i-(string of exeactly 17 characters)
+	// if they get longer like they have had a history of doing this will continue to work, essentially
+	// saying that they must be at least as long as they are currently.
+	matched, err := regexp.MatchString("^i-[[:alnum:]]{8,}$", id)
+	if err != nil {
+		return false, err
+	}
+	return matched, nil
+}
+
+func (e *EnvInfoGroup) getNullInfo() error {
+	e.CloudProvider = "unknown"
+	e.CloudProviderInfo = map[string]string{}
+	return nil
+}
+
+func (e *EnvInfoGroup) getAwsInfo() error {
+	instId, err := hitAwsMagicIp("instance-id")
+	if err != nil {
+		return err
+	}
+	instType, err := hitAwsMagicIp("instance-type")
+	if err != nil {
+		return err
+	}
+	e.CloudProvider = "aws"
+	e.CloudProviderInfo = map[string]string{}
+	e.CloudProviderInfo["instance-id"] = instId
+	e.CloudProviderInfo["instance-type"] = instType
+	return nil
+}
+
+func (e *EnvInfoGroup) String() string {
+	s, _ := json.Marshal(e)
+	return string(s)
+}
+
+func (e *EnvInfoGroup) GetInfo() {
+	if ans, _ := IAmInAws(); ans {
+		e.getAwsInfo()
+		jww.DEBUG.Println("detected aws environment")
+	} else {
+		e.getNullInfo()
+		jww.DEBUG.Println("detected unknown environment")
+	}
+}
+
+func GetInfo() plugin.InfoGroup {
+	info := new(EnvInfoGroup)
+	info.GetInfo()
+	return info
+}
+
+func String() string {
+	info := new(EnvInfoGroup)
+	info.GetInfo()
+	return info.String()
 }
