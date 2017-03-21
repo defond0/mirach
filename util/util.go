a464c8c63495a7299f729d163c71268017d37c84
Jeff DeFond <jeff.defond@cleardata.com>
2017-03-21 12:15:23 -0700

    Clean up old util method

--- util/util.go
+++ util/util.go
@@ -2,10 +2,7 @@ package util
 
 import (
 	"fmt"
-	"io/ioutil"
-	"net/http"
 	"path/filepath"
-	"regexp"
 	"time"
 	"unicode/utf8"
 
@@ -166,36 +163,4 @@ func Timeout(d time.Duration) <-chan bool {
 		ch <- true
 	}()
 	return ch
-}
-
-//WhereAmI returns a string representing the name of the cloud provider, or err
-// if they do not use a cloud provider in the following list:
-//       aws
-func WhereAmI() (string, error) {
-	if ans, err := IAmInAws(); ans {
-		return "aws", err
-	}
-	return "none", nil
-}
-
-// IAmInAws returns a bool, is mirach in aws?
-func IAmInAws() (bool, error) {
-	res, err := http.Get("http://169.254.169.254/latest/meta-data/instance-id")
-	if err != nil {
-		return false, nil
-	}
-	defer res.Body.Close()
-	if res.StatusCode == 200 { // OK
-		bodyBytes, err := ioutil.ReadAll(res.Body)
-		if err != nil {
-			return false, err
-		}
-		// this will match instances ids we could force them to be i-(string of exeactly 17 characters)
-		// if they get longer like they have had a history of doing this will continue to work, essentially
-		// saying that they must be at least as long as they are currently.
-		matched, err := regexp.MatchString("i-[[:alnum:]]{17}", string(bodyBytes))
-		return matched, err
-	}
-	return false, err
-
 }
