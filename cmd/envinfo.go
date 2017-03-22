10a489aad8562435e71849d5bef33f47b5a90521
Jeff DeFond <jeff.defond@cleardata.com>
2017-03-22 12:06:33 -0700

    Update help text to make sense

--- cmd/envinfo.go
+++ cmd/envinfo.go
@@ -9,10 +9,9 @@ import (
 
 var envinfoCmd = &cobra.Command{
 	Use:   "envinfo",
-	Short: "Run one of mirach's built in envinfo plugin.",
+	Short: "Run one of built in envinfo plugin.",
 	Long: "mirach plugins are primarily used from within mirach, but this allows " +
-		"you to run this one directly directly. It will return a json string of " +
-		"the type passed in with the -i switch or system information by default.",
+		"you to run this one directly directly.",
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println(envinfo.String())
 	},
