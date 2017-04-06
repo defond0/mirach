87f22221ab1958f8e89c9ab8bf1dcc5c54e52b91
Jeff DeFond <jeff.defond@cleardata.com>
2017-04-06 14:25:39 -0700

    Update yum.go function signatures to include maps

--- plugin/pkginfo/parsers/yum.go
+++ plugin/pkginfo/parsers/yum.go
@@ -3,9 +3,9 @@ package parsers
 import "os/exec"
 
 // GetYumPkgs creates map of available, installed and available security packages from yum as well as a list of errors that occurred generating that list. grep returns exit status 1 when it gets no match, errors like that are fine
-func GetYumPkgs() (map[string][]LinuxPackage, []error) {
+func GetYumPkgs() (map[string]map[string]LinuxPackage, []error) {
 	errors := []error{}
-	out := make(map[string][]LinuxPackage)
+	out := make(map[string]map[string]LinuxPackage)
 	avail, err := getYumAvailablePackages()
 	if err != nil {
 		errors = append(errors, err)
@@ -24,7 +24,7 @@ func GetYumPkgs() (map[string][]LinuxPackage, []error) {
 	return out, errors
 }
 
-func getYumInstalledPackages() ([]LinuxPackage, error) {
+func getYumInstalledPackages() (map[string]LinuxPackage, error) {
 	yum := command("yum list installed -q")
 	grep := exec.Command("grep", "-v", "Installed Packages")
 	awk := exec.Command("awk", "{{ print $1 , $2 }}")
@@ -35,7 +35,7 @@ func getYumInstalledPackages() ([]LinuxPackage, error) {
 	return parsePacakgesFromBytes(stdout, false)
 }
 
-func getYumAvailablePackages() ([]LinuxPackage, error) {
+func getYumAvailablePackages() (map[string]LinuxPackage, error) {
 	yum := command("yum list updates -q")
 	grep := exec.Command("grep", "-v", "Updated Packages")
 	awk := exec.Command("awk", "{{ print $1 , $2 }}")
@@ -46,7 +46,7 @@ func getYumAvailablePackages() ([]LinuxPackage, error) {
 	return parsePacakgesFromBytes(stdout, false)
 }
 
-func getYumAvailableSecurityPackages() ([]LinuxPackage, error) {
+func getYumAvailableSecurityPackages() (map[string]LinuxPackage, error) {
 	yum := command("yum list updates -q --security")
 	grep := exec.Command("grep", "-v", "Updated Packages")
 	awk := exec.Command("awk", "{{ print $1 , $2 }}")
