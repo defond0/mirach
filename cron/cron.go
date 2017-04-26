508e76d5508e3dd5ccf96542c934694dd63401d8
Herkermer Sherwood <herkermer.sherwood@cleardata.com>
2017-04-25 19:30:06 -0700

    Add method to cron to add a function with a random delay, minimum not required

--- cron/cron.go
+++ cron/cron.go
@@ -2,6 +2,7 @@
 package cron
 
 import (
+	"math/rand"
 	"time"
 
 	"github.com/robfig/cron"
@@ -19,6 +20,9 @@ func New() *MirachCron {
 	return &MirachCron{c}
 }
 
+// AddFuncDelayed delays the call to AddFunc by the given delay.
+// You must also pass in a chan for the purpose of returning either a
+// result or an err that the caller can chose to utilize.
 func (c *MirachCron) AddFuncDelayed(spec string, cmd func(), delay time.Duration, res chan<- interface{}) {
 	go func() {
 		time.Sleep(delay)
@@ -27,4 +31,27 @@ func (c *MirachCron) AddFuncDelayed(spec string, cmd func(), delay time.Duration
 		}
 		res <- nil
 	}()
+}
+
+// AddFuncRandDelay delays the call to AddFunc by a random delay.
+// You must also pass in a chan for the purpose of returning either a
+// result or an err that the caller can chose to utilize. You must
+// provide at least one time.Duration parameter and optionally a second.
+// The first duration parameter will be the maximum delay. The second,
+// if provided will be the minimum time to wait. Remaining arguments
+// are ignored.
+func (c *MirachCron) AddFuncRandDelay(spec string, cmd func(), res chan<- interface{}, t ...time.Duration) {
+	var max, min int64
+	for i, x := range t {
+		switch i {
+		case 0:
+			max = int64(x)
+		case 1:
+			min = int64(x)
+			break
+		}
+	}
+	rand.Seed(time.Now().UTC().UnixNano())
+	delay := time.Duration(rand.Int63n(max-min) + min)
+	c.AddFuncDelayed(spec, cmd, delay, res)
 }
