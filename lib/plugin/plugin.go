b60db3682a90217661fb5b26fd1312cc80f49061
Jeff DeFond <jeff.defond@cleardata.com>
2017-11-06 08:24:23 -0800

    Update plugin go, psuedo code out at a function to wrap runner interface in cron interface

--- lib/plugin/plugin.go
+++ lib/plugin/plugin.go
@@ -1,29 +1,49 @@
 package plugin
 
 import (
+	"encoding/json"
 	"io"
 
 	"github.com/cleardataeng/mirach/lib/cron"
 )
 
+type Runner interface {
+	Run() []byte
+}
+
 // Plugin is a routine used to collect data.
-type Plugin struct {
-	Disabled  bool
-	Label     string
+type PlugConf struct {
+	Name      string
 	LoadDelay string `mapstructure:"load_delay"`
 	RunAtLoad bool   `mapstructure:"run_at_load"`
 	Schedule  string
-	Type      string
+	Plugin    Runner
+}
+
+func (p PlugConf) cronStat() {
+	bytes := p.Plugin.Run()
+	res := struct {
+		name string
+		data []byte
+	}{
+		name: p.Name,
+		data: bytes,
+	}
+	realres := json.Marshal(res)
+	for _, b := range p.Backend {
+		b.Write(realres)
+	}
 }
 
 type Ctrl struct {
+	plugins []PlugConf
 	cron    *cron.MirachCron
-	backend []io.ReadWriter
+	backend map[PlugConf][]io.ReadWriter
 }
 
-func NewCtrl(c *cron.MirachCron, b []io.ReadWriter) *Ctrl {
+func NewCtrl(b []io.ReadWriter) *Ctrl {
 	return &Ctrl{
-		cron:    c,
+		cron:    cron.New(),
 		backend: b,
 	}
 }
